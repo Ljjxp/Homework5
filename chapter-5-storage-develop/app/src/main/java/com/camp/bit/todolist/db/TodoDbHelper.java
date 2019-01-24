a77b2ce3b32ce52caa64ef01ab377cee94d5ed7b
ljjxp <liujunjie1224@gmail.com>
2019-01-24 09:25:34 +0800

    Exercise5_1

---
+++ chapter-5-storage-develop/app/src/main/java/com/camp/bit/todolist/db/TodoDbHelper.java
@@ -0,0 +1,34 @@
+package com.camp.bit.todolist.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.camp.bit.todolist.db.TodoContract.SQL_CREATE_ENTRIES;
+import static com.camp.bit.todolist.db.TodoContract.SQL_DELETE_ENTRIES;
+
+/**
+ * Created on 2019/1/22.
+ *
+ * @author xuyingyi@bytedance.com (Yingyi Xu)
+ */
+public class TodoDbHelper extends SQLiteOpenHelper {
+
+    // TODO 定义数据库名、版本；创建数据库
+
+    public TodoDbHelper(Context context) {
+        super(context, "todo", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+}
